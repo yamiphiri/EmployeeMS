f888c4d5dcda3b6d15caa21b33a15ef1bb3f7b1d
Yami Phiri <yphiri@cc.ac.mw>
2020-03-19 23:16:49 +0200

    added arrays to work with object

--- src/Employee.java
+++ src/Employee.java
@@ -4,6 +4,8 @@
 public class Employee {
     //object attribute
     private String name;
+    private double [] numberOfWorkingHours;
+    private final double  rate =12;
     //no args construtor
     public Employee(){
 
@@ -16,4 +18,32 @@ public class Employee {
     public String getName() {
         return name;
     }
+
+    // this method set number of working hours for the object
+    // it si made to receive an array as a parameter declared in this case ad double [] workingHours
+    public void setNumberOfWorkingHours(double[] numberOfWorkingHours) {
+        this.numberOfWorkingHours = numberOfWorkingHours;
+    }
+    //this method return or retrieves or get number of working hours of the employee
+    //its return type is double [] meaning that it is expected to return an array containing element of double data type in this case working hours
+    public double[] getNumberOfWorkingHours() {
+        return numberOfWorkingHours;
+    }
+    //this method calculate pay of the employee
+    public double getPay(){
+        double total = 0;
+        for (int i = 0; i <this.numberOfWorkingHours.length ; i++) {
+            total =total + this.numberOfWorkingHours[i];
+
+
+        }
+        /* you can also implement it as this
+        for (int i = 0; i <getNumberOfWorkingHours().length ; i++) {
+            total =total + getNumberOfWorkingHours()[i];
+
+        }*/
+        return this.rate * total;
+
+    }
+
 }
--- src/EmployeeManagementSystem.java
+++ src/EmployeeManagementSystem.java
@@ -19,5 +19,29 @@ public class EmployeeManagementSystem {
         empl1.setName(n);
         //printing the name of the employee using set method
         System.out.println("from the Obj Name is "+ empl1.getName());
+
+        //decraring an array to hold number of hours for five day
+        double [] workingHours = new double[5];
+        // loop to get number of hours
+        for (int i = 0; i <workingHours.length ; i++) {
+            // since arrays are zero based but number of days start with 1 this will make sure that the number of days are starting from 1 to 5 while th loop is counting from 0 to 4
+            int day = i+1;
+            //telling the user to enter the number of days
+            System.out.println("enter your number of working hours at day "+ day );
+            // getting the number of day and keeping them in an array
+            workingHours[i] =input.nextDouble();
+        }
+        //passing the array 'woringHours' to a method in the emp1 object that expects an array in order to set working hours
+        empl1.setNumberOfWorkingHours(workingHours);
+
+        //calculating the total hours: this must be implemented in empleyees class not here
+        double totalWorkingHours = 0;
+        for (int i = 0; i <empl1.getNumberOfWorkingHours() .length ; i++) {
+            totalWorkingHours =totalWorkingHours + empl1.getNumberOfWorkingHours() [i];
+        }
+        //printing amount to pay our employee
+        System.out.println( empl1.getName() +" your total working hours is "+totalWorkingHours+" you payement amounts to K"+empl1.getPay());
+
+
     }
 }
